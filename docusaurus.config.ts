2c740e2909c1ae9f7f79fed769e88f43d961dc76
皮皮萌宝 <panda1943575780@outlook.com>
2025-08-02 11:30:28 +0800

    Update Docusaurus configuration for GitHub Pages deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://pandaymx.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/study-computer/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'pandaymx', // GitHub org/user name.
+  projectName: 'study-computer', // Repository name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
